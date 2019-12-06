0b6f443255e34ac9759d7c6326545133c7c2cf13
Jingtao-ZHANG <jt.z@outlook.com>
2019-12-06 15:30:44 +0800

    add customized filter

--- Lecture4_smoothing/main.cpp
+++ Lecture4_smoothing/main.cpp
@@ -33,6 +33,37 @@ int main() {
         GaussianBlur(srcImage, ImageByGaussianFilter, Size(5, 5), 0.8, 0.8);
         imshow("smoothed by Gaussian filter", ImageByGaussianFilter);
 
+        /*
+         * Above-mentioned filters are low pass filters, which are linear operators.
+         * Then, a media pass filter is presented.
+         * MedianBLur is a non-linear operator, where the statistical median of each point in the neighborhood is
+         * the output. This filter can effectively remove salt and pepper noise. It also keeps the boundaries of
+         * the objects in the image from being blurred.
+         */
+
+        // example of median blur filter
+        Mat ImageByMedianBlur;
+        medianBlur(srcImage, ImageByMedianBlur, 7);
+        imshow("smoothed by median blur filter", ImageByMedianBlur);
+
+        /*
+         * We can customize the filter kernel using the function of "cv::filter2D" or "cv::sepFilter2D".
+         * The "filter2D" uses a square kernel, while "sepFilter2D" uses a X kernel and a Y kernel.
+         */
+        // example of customize a Gaussian filter
+        Mat GaussianKernel3By1 = getGaussianKernel(5, 0.8);  // get a one dimension Gaussian kernel
+        Mat GaussianKernel1By3 = GaussianKernel3By1.t();
+        // use filter2D
+        Mat CustomizedGaussianKernel = GaussianKernel3By1 * GaussianKernel1By3;
+        cout << CustomizedGaussianKernel << endl;
+        Mat ImageByCustomizedKernel1;
+        filter2D(srcImage, ImageByCustomizedKernel1, srcImage.depth(), CustomizedGaussianKernel);
+        imshow("smoothed by customized filter using square kernel", ImageByCustomizedKernel1);
+        // use seqFilter2D
+        Mat ImageByCustomizedKernel2;
+        sepFilter2D(srcImage, ImageByCustomizedKernel2, srcImage.depth(), GaussianKernel3By1, GaussianKernel1By3);
+        imshow("smoothed by customized filter using separate kernel", ImageByCustomizedKernel2);
+
         waitKey(0);
 
         return 0;
